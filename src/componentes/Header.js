063b2945e5f6a3233590bf80b20a5246ec144450
Android Studio <devandroid@ort>
2022-09-01 10:23:30 -0300

    proyecto creado

---
+++ src/componentes/Header.js
@@ -0,0 +1,14 @@
+import React, {} from "react";
+import '../App.css';
+
+export default function Header({}){
+    return(
+        <nav>
+            <div class="nav-wrapper light-blue darken-2">
+             <a href="#!" class="brand-logo">Clima React APP</a>
+            </div>
+        </nav>
+      )
+}; 
+
+
