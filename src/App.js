9a21dc3a7bf7b03c03082b0f9dd4ff7b55665d69
Android Studio <devandroid@ort>
2022-09-22 08:58:11 -0300

    xd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from './componentes/Header';
+import Header from './componentes/Navbar';
 import Form from './componentes/Form';
 import Card from './componentes/Card';
 import React, {useState} from 'react';
@@ -7,28 +7,27 @@ import './App.css';
 function App() {
 
   const [lugar, setLugar] = useState(
-    {
-        pais:"",
-        ciudad:""
-    }
-)
+  {
+    pais:"",
+    ciudad:""
+  }
+  )
+
   return (
     <>
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          <Header/>
-          <div style={{display: 'flex', flexDirection: 'row'}}>
-          <div style={{flex: 1}}>
-          <Form lugar={lugar} setLugar={setLugar}/>
-          </div>
-          <div style={{flex: 1}}>
-          <Card lugar={lugar}/>
-          </div>
+      <Header/>
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div class="col m6 s12">
+              <Form lugar={lugar} setLugar={setLugar}/>
+            </div>
+            <div class="col m6 s12">
+              <Card lugar={lugar}/>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
--- src/componentes/Form.js
+++ src/componentes/Form.js
@@ -14,54 +14,34 @@ const Form = ({setLugar}) => {
             [e.target.name]: e.target.value
         })
     }
-
+    
     return (
-        <div className="contenedor-form">
-            <div className="container">
-                <div className="row">
-                    <div className="col m6 s12">
-                        <form>
-                            <div className="input-field col s12">
-                                <label htmlFor="ciudad" className="active">Ciudad:</label>
-                                <input type="text" name="ciudad" id="ciudad" onChange={handleChange} style={{color: '#000'}}></input>
-                            </div>
-                            <div className="input-field col s12">
-                                <div className="select-wrapper">
-                                    {/* <select className="select-dropdown dropdown-trigger" name="pais" type="" readOnly={true} data-target="select-options-8d2743a6-734a-067b-9260-f2222257c15e" onChange={handleChange}>
-                                        <option id="select-options-8d2743a6-734a-067b-9260-f2222257c15e0" tabIndex="0" className="selected"><span>-- Seleccion un Pais --</span></option>
-                                        <option id="select-options-8d2743a6-734a-067b-9260-f2222257c15e1" tabIndex="0"><span>Estados Unidos</span></option>
-                                        <option id="select-options-8d2743a6-734a-067b-9260-f2222257c15e2" tabIndex="0"><span>México</span></option>
-                                        <option id="select-options-8d2743a6-734a-067b-9260-f2222257c15e3" tabIndex="0"><span>Argentina</span></option>
-                                        <option id="select-options-8d2743a6-734a-067b-9260-f2222257c15e4" tabIndex="0"><span>Colombia</span></option>
-                                        <option id="select-options-8d2743a6-734a-067b-9260-f2222257c15e5" tabIndex="0"><span>Costa Rica</span></option>
-                                        <option id="select-options-8d2743a6-734a-067b-9260-f2222257c15e6" tabIndex="0"><span>España</span></option>
-                                        <option id="select-options-8d2743a6-734a-067b-9260-f2222257c15e7" tabIndex="0"><span>Perú</span></option>
-                                    </select>
-                                    <svg className="caret" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M7 10l5 5 5-5z"></path>
-                                        <path d="M0 0h24v24H0z" fill="none"></path>
-                                    </svg> */}
-                                    <label htmlFor="pais">Pais:</label>
-                                    <select name="pais" id="pais" tabIndex="-1" onChange={handleChange}>
-                                        <option value="">-- Seleccion un Pais --</option>
-                                        <option value="US">Estados Unidos</option>
-                                        <option value="MX">México</option>
-                                        <option value="AR">Argentina</option>
-                                        <option value="CO">Colombia</option>
-                                        <option value="CR">Costa Rica</option>
-                                        <option value="ES">España</option>
-                                        <option value="PE">Perú</option>
-                                    </select>
-                                </div>
-                                
-                            </div>
-                            
-                            <button onClick={ () => {setLugar(lugarForm)}}>Buscar Clima</button>
-                        </form>
-                    </div>
+            <form>
+                <div className="input-field col s12">
+                    <label htmlFor="ciudad" className="active">Ciudad:</label>
+                    <input type="text" name="ciudad" id="ciudad" onChange={handleChange} style={{ color: '#000' }}></input>
                 </div>
-            </div>
-        </div>
+                <div className="input-field col s12">
+                    <div className="select-wrapper">
+                        <label htmlFor="pais">Pais:</label>
+                        <select name="pais" id="pais" tabIndex="-1" onChange={handleChange}>
+                            <option value="">-- Seleccion un Pais --</option>
+                            <option value="US">Estados Unidos</option>
+                            <option value="MX">México</option>
+                            <option value="AR">Argentina</option>
+                            <option value="CO">Colombia</option>
+                            <option value="CR">Costa Rica</option>
+                            <option value="ES">España</option>
+                            <option value="PE">Perú</option>
+                        </select>
+                    </div>
+
+                </div>
+                
+
+                <button onClick={() => { setLugar(lugarForm) }}>Buscar Clima</button>
+            </form>
+
     )
 }
 
